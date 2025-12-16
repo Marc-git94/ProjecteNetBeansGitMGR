4728fcde9ffffac4c84fc42ac079c23d590fd10f
Marc-git94 <marcgr2007@gmail.com>
2025-12-16 10:13:33 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/projectenetbeansgitmgr/ProjecteNetBeansGitMGR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectenetbeansgitmgr;
+
+/**
+ *
+ * @author Marc
+ */
+public class ProjecteNetBeansGitMGR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
